e6c158b92867ecb8f43a18a88fe99b4007833a10
Monte Kietpawpan <kietpawpan@gmail.com>
2024-07-19 13:55:56 +0700

    Add files via upload

---
+++ ref.js
@@ -0,0 +1,20 @@
+function r1() {
+  var popup = document.getElementById("r1");
+  popup.classList.toggle("show");
+}
+
+function r2() {
+  var popup = document.getElementById("r2");
+  popup.classList.toggle("show");
+}
+
+function r5() {
+  var popup = document.getElementById("r5");
+  popup.classList.toggle("show");
+}
+
+function r4() {
+  var popup = document.getElementById("r4");
+  popup.classList.toggle("show");
+}
+
